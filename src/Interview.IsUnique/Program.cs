dab242416260788bb99d171e291739ce75d38060
sbabacan <sbabacan@HN-PC39>
2021-01-26 16:22:36 +0300

    init.

---
+++ src/Interview.IsUnique/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interview.IsUnique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
