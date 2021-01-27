a838b916f714fcdb0299a904c6b3d66472d5aeb9
Sinan Babacan <sinanbabacan@gmail.com>
2021-01-27 09:43:07 +0300

    IsUnique

--- src/Interview.IsUnique/Program.cs
+++ src/Interview.IsUnique/Program.cs
@@ -6,7 +6,28 @@ namespace Interview.IsUnique
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(IsUnique("sinan"));
+            Console.WriteLine(IsUnique("miray"));
+            Console.WriteLine(IsUnique("öğretmen"));
+            Console.WriteLine(IsUnique("öğretim"));
+            Console.WriteLine(IsUnique("derya"));
+        }
+
+        private static bool IsUnique(string input)
+        {
+            bool[] characters = new bool[Char.MaxValue];
+
+            foreach (Char item in input)
+            {
+                if (characters[item])
+                {
+                    return false;
+                }
+
+                characters[item] = true;
+            }
+
+            return true;
         }
     }
 }
