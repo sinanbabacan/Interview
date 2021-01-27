4c4824294b2bcb2ab8193f809da85c4b7675b008
Sinan Babacan <sinanbabacan@gmail.com>
2021-01-27 09:58:45 +0300

    Check Permutation

---
+++ src/Interview.CheckPermutation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Interview.CheckPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Check("god","dog"));
+            Console.WriteLine(Check("God","Dog"));
+            Console.WriteLine(Check("pen","pencil"));
+            Console.WriteLine(Check("sinan", "nanis"));
+
+        }
+
+        private static bool Check(string input1, string input2)
+        {
+            if (input1.Length != input2.Length)
+            {
+                return false;
+            }
+
+            int[] characterCounter = new int[Char.MaxValue];
+
+            foreach (Char item in input1)
+            {
+                characterCounter[item]++;
+            }
+
+            foreach (Char item in input2)
+            {
+                characterCounter[item]--;
+
+                if (characterCounter[item] < 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
