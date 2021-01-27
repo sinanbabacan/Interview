a03fd0f386a0f0883d8daf016a3ae34273d2ee2e
Sinan Babacan <sinanbabacan@gmail.com>
2021-01-28 00:07:58 +0300

    OneWay

---
+++ src/Interview.OneWay/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Interview.OneWay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             
+            pale, ple -> true
+            pales, pale -> true
+            pale, bale -> true
+            pale, bake -> false 
+             
+             */
+
+            //Console.WriteLine(Check("pale", "ple"));
+            Console.WriteLine(Check("pales", "pale"));
+            Console.WriteLine(Check("palesi", "pale"));
+            Console.WriteLine(Check("pale", "bale"));
+            Console.WriteLine(Check("pale", "bake"));
+            Console.WriteLine(Check("pale", "pale"));
+
+        }
+
+        private static bool Check(string input, string modifiedInput)
+        {
+            if (input.Length == modifiedInput.Length)
+            {
+                bool updated = false;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] != modifiedInput[i])
+                    {
+                        if (updated)
+                        {
+                            return false;
+                        }
+
+                        updated = true;
+                    }
+                }
+            }
+            else if(Math.Abs(input.Length - modifiedInput.Length) == 1)
+            {
+                int i = 0;
+                int j = 0;
+                bool updated = false;
+
+                do
+                {
+                    if (modifiedInput[j] != input[i])
+                    {
+                        if (updated)
+                        {
+                            return false;
+                        }
+
+                        updated = true;
+                    }
+                    else
+                    {
+                        j++;
+                    }
+
+                    i++;
+
+                } while (j < modifiedInput.Length);
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
