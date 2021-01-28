9a5dbaee83b4cc571c91c0970c9f215e3634c86b
sbabacan <sbabacan@HN-PC39>
2021-01-28 15:09:25 +0300

    StringCompression

---
+++ src/Interview.StringCompression/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace Interview.StringCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Compress("aabcccccaaazzzzzz"));
+            Console.WriteLine(Compress("aabccccdefgghijzz"));
+        }
+
+        private static string Compress(string input)
+        {
+            var stringBuilder = new StringBuilder();
+
+            int counter = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                counter++;
+
+                if (i + 1 == input.Length || input[i] != input[i + 1])
+                {
+                    stringBuilder.Append(input[i]);
+                    stringBuilder.Append(counter);
+
+                    counter = 0;
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
