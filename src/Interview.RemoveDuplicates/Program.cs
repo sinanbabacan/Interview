578a8f711e4ba5fc762bf7b33876082f0b68b9ee
Sinan Babacan <sinanbabacan@gmail.com>
2021-01-31 00:51:41 +0300

    development.

---
+++ src/Interview.RemoveDuplicates/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interview.RemoveDuplicates
+{
+    public class Node
+    {
+        public Node Next { get; set; }
+
+        public int Value { get; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node head = new Node(5);
+            head.Next = new Node(4);
+            head.Next.Next = new Node(3);
+            head.Next.Next.Next = new Node(3);
+            head.Next.Next.Next.Next = new Node(2);
+            head.Next.Next.Next.Next.Next = new Node(1);
+            head.Next.Next.Next.Next.Next.Next = new Node(3);
+            head.Next.Next.Next.Next.Next.Next.Next = new Node(1);
+            head.Next.Next.Next.Next.Next.Next.Next.Next = new Node(1);
+            head.Next.Next.Next.Next.Next.Next.Next.Next.Next = new Node(3);
+
+            // RemoveDups(head);
+            RemoveDups2(head);
+        }
+
+
+        private static void RemoveDups(Node node)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            Node previous = null;
+
+            while (node != null)
+            {
+                if (set.Contains(node.Value))
+                {
+                    previous.Next = node.Next;
+                }
+                else
+                {
+                    set.Add(node.Value);
+                    previous = node;
+                }
+
+                node = node.Next;
+            }
+        }
+
+        // fast runner algorithm
+        private static void RemoveDups2(Node node)
+        {
+            Node current = node;
+
+            while (current != null)
+            {
+                Node runner = current;
+
+                while (runner.Next != null)
+                {
+                    if (current.Value == runner.Next.Value)
+                    {
+                        runner.Next = runner.Next.Next;
+                    }
+                    else
+                    {
+                        runner = runner.Next;
+                    }
+                }
+
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ src/Interview.StringRotation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Interview.StringRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Check("erbottlewat", "waterbottle"));
+        }
+
+        private static bool Check(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            char[] arr1 = s1.ToCharArray();
+            char[] arr2 = s2.ToCharArray();
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                if (Enumerable.SequenceEqual(arr1, arr2))
+                {
+                    return true;
+                }
+
+                char temp = arr2[0];
+
+                for (int j = 0; j < arr2.Length - 1; j++)
+                {
+                    arr2[j] = arr2[j + 1];
+                }
+
+                arr2[arr2.Length - 1] = temp;
+            }
+
+            return false;
+        }
+    }
+}
