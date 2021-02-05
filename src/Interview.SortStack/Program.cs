914cff123760842d89445c6c6a6f06b63e183175
Sinan Babacan <sinanbabacan@gmail.com>
2021-02-05 12:59:51 +0300

    SortedStack

--- src/Interview.SortStack/Program.cs
+++ src/Interview.SortStack/Program.cs
@@ -7,17 +7,56 @@ namespace Interview.SortStack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SortedStack sortedStack = new SortedStack();
+
+            sortedStack.Push(4);
+            sortedStack.Push(5);
+            sortedStack.Push(2);
+            sortedStack.Push(6);
+            sortedStack.Push(7);
+
+            
+
         }
     }
 
     public class SortedStack
     {
-        Stack<int> stack;
+        Stack<int> _stack;
+        Stack<int> _temp;
         public SortedStack()
         {
-            stack = new Stack<int>();
+            _stack = new Stack<int>();
+            _temp = new Stack<int>();
         }
+
+        public void Push(int val)
+        {
+            while (_stack.Count > 0 && val > _stack.Peek())
+            {
+                _temp.Push(_stack.Pop());
+            }
+
+            _stack.Push(val);
+
+            while (_temp.Count > 0)
+            {
+                _stack.Push(_temp.Pop());
+            }
+        }
+
+        public int Pop()
+        {
+            return _stack.Pop();
+        }
+
+        public int Peek()
+        {
+
+            return _stack.Peek();
+        }
+
+       
     }
 
 }
