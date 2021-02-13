2e77b84baca268ce33205d121897b0e451d47b32
Sinan Babacan <sinanbabacan@gmail.com>
2021-02-13 17:11:24 +0300

    dev.

---
+++ src/Interview.AnimalShelter/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interview.AnimalShelter
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Shelter shelter = new Shelter();
+
+            shelter.Enqueue(new Animal() { Type = AnimalType.Cat, JoinDate = DateTime.Now.AddDays(-1) });
+            shelter.Enqueue(new Animal() { Type = AnimalType.Cat, JoinDate = DateTime.Now.AddDays(-2) });
+            shelter.Enqueue(new Animal() { Type = AnimalType.Cat, JoinDate = DateTime.Now.AddDays(-3) });
+            shelter.Enqueue(new Animal() { Type = AnimalType.Dog, JoinDate = DateTime.Now.AddDays(-4) });
+            shelter.Enqueue(new Animal() { Type = AnimalType.Cat, JoinDate = DateTime.Now.AddDays(-5) });
+            shelter.Enqueue(new Animal() { Type = AnimalType.Cat, JoinDate = DateTime.Now.AddDays(-6) });
+            shelter.Enqueue(new Animal() { Type = AnimalType.Dog, JoinDate = DateTime.Now.AddDays(-7) });
+
+
+            Animal animal = shelter.Dequeue(AnimalType.Dog);
+            Animal animal2 = shelter.Dequeue(AnimalType.Dog);
+            Animal animal3 = shelter.Dequeue(AnimalType.Dog);
+
+        }
+
+        public enum AnimalType
+        {
+            Cat,
+            Dog
+        }
+
+        public class Animal
+        {
+            public DateTime JoinDate { get; set; }
+            public AnimalType Type { get; set; }
+        }
+
+        public class Shelter
+        {
+            LinkedList<Animal> _shelter;
+
+            public Shelter()
+            {
+                _shelter = new LinkedList<Animal>();
+            }
+
+            public void Enqueue(Animal animal)
+            {
+                _shelter.AddLast(animal);
+            }
+
+            public Animal DequeueAny()
+            {
+                if (_shelter.Count > 0)
+                {
+                    Animal value = _shelter.First.Value;
+
+                    _shelter.RemoveFirst();
+
+                    return value;
+                }
+
+                return null;
+            }
+
+            public Animal Dequeue(AnimalType type)
+            {
+                LinkedListNode<Animal> node = null;
+
+                if (_shelter.Count > 0)
+                {
+                    node = _shelter.First;
+
+                    while (node != null)
+                    {
+                        if (node.Value.Type == type)
+                        {
+                            _shelter.Remove(node);
+
+                            break;
+                        }
+
+                        node = node.Next;
+                    }
+                }
+
+                return node?.Value;
+            }
+        }
+    }
+}
---
+++ src/Interview.RouteBetweenNodes/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using QuickGraph;
+
+namespace Interview.RouteBetweenNodes
+{
+    public class Node
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public Node(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+
+    public class Edge<Node> : IEdge<Node>
+    {
+
+        public Edge(Node source, Node target)
+        {
+            Source = source;
+            Target = target;
+        }
+
+        public Node Source { get; }
+
+        public Node Target { get; }
+    }
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            AdjacencyGraph<Node, Edge<Node>> adjacencyGraph1 = new AdjacencyGraph<Node, Edge<Node>>();
+
+            AdjacencyGraph<int, IEdge<int>> adjacencyGraph2 = new AdjacencyGraph<int, IEdge<int>>();
+
+            Node n1 = new Node("A", 1);
+            Node n2 = new Node("B", 2);
+            Node n3 = new Node("C", 3);
+            Node n4 = new Node("D", 4);
+
+            var edge = new Edge<Node>(n1, n2);
+            var edge2 = new Edge<Node>(n3, n4);
+
+            adjacencyGraph1.AddVertex(n1);
+            adjacencyGraph1.AddVertex(n2);
+
+            adjacencyGraph1.AddEdge(edge);
+            adjacencyGraph1.AddEdge(edge2);
+
+            Edge<Node> edge1 = adjacencyGraph1.Edges.Last();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
